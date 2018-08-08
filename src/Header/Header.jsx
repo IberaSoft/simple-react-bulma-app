c5a3a22abe854f26a3c67829265ca6762cc6e838
Juan Llorens <juan.llorens@williamhill.com>
2018-08-08 10:14:10 +0200

    Added layout components

---
+++ src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './../Assets/logo.svg';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <img src={logo} className="logo" alt="logo" />
+                <h1 className="header-title">Welcome to React</h1>
+            </header>
+        );
+    }
+}
+
+export default Header
---
+++ src/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <main className="main">
+                Main content
+            </main>
+        );
+    }
+}
+
+export default Main;
