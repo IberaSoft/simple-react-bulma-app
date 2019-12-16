ed69cdfadc6f8f17556f3373e5ee799e2ea2c9e6
Juan Llorens <juan.llorens@williamhill.com>
2019-12-16 14:47:32 +0100

    Build updated

---
+++ build/precache-manifest.8c899294ce335f1a3c807f0354c18cfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87188746edca3f676c1e810b7c96d5cc",
+    "url": "/simple-react-bulma-app/index.html"
+  },
+  {
+    "revision": "6a4dc09bd1ced6eab241",
+    "url": "/simple-react-bulma-app/static/css/main.53020a54.chunk.css"
+  },
+  {
+    "revision": "6de5c5d8fcb12739eb29",
+    "url": "/simple-react-bulma-app/static/js/2.35fdb4e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/simple-react-bulma-app/static/js/2.35fdb4e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a4dc09bd1ced6eab241",
+    "url": "/simple-react-bulma-app/static/js/main.60066949.chunk.js"
+  },
+  {
+    "revision": "145e9ba2f2ae8b9f28af",
+    "url": "/simple-react-bulma-app/static/js/runtime-main.2369da31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-react-bulma-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
